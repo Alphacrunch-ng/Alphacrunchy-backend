0954958237202f88e3a5f1e08ac05291961a09de
Vastro-lorde <omatsolaseund@gmail.com>
2023-01-16 02:38:43 +0100

    cleaning up wallet controller

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -100,34 +100,3 @@ exports.getWallets = async (request, response,) => {
         return serverError(res, error);
     }
 }
-
-// controller for updating a User
-exports.updateUser = async (req, res) => {
-    try {
-        const checkUser = await User.findOne({ id: req.params.id});
-        if (!checkUser) {
-            return res.status(204).json({
-                status: 'failed',
-                message: 'user not found'
-            });
-        }
-
-        // checking the cloudiinary upload from multer
-        const cloudFile = await cloudinary.uploader.upload(req.file.path)
-        // Retrieving profile pic cloudinary public id if it exists.
-        if (checkUser.profilePic_cloudId) {
-           await cloudinary.uploader.destroy(checkPic.profilePic_cloudId)
-        }
-        const {fullName, sex, phoneNumber, city, state, address} = req.body;
-        const profilePicture_url = cloudFile.secure_url;
-        const profilePic_cloudId = cloudFile.public_id;
-
-        const user = await User.findByIdAndUpdate({ id: checkUser._id},{fullName, sex, phoneNumber, city, state, address, profilePicture_url, profilePic_cloudId}, {new: true}).select("-password");
-        return res.status(200).json({
-            status: 'success',
-            data: user
-        });
-    } catch (error) {
-        return serverError(res, error);
-    }
-}
