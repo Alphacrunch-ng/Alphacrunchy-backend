85fc2f5eee5949509f5a6deb6160e769c9db33aa
Vastro-lorde <omatsolaseund@gmail.com>
2023-05-31 15:41:09 +0100

    fixed update user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -123,6 +123,7 @@ exports.getUsers = async (request, response) => {
 // controller for updating a User
 exports.updateUser = async (req, res) => {
     try {
+        const {fullName, sex, phoneNumber, city, state, address} = req.body;
         const checkUser = await User.findOne({ id: req.params.id});
         if (!checkUser) {
             return res.status(204).json({
@@ -130,18 +131,19 @@ exports.updateUser = async (req, res) => {
                 message: 'user not found'
             });
         }
+        if (req.file) {
+            // checking the cloudiinary upload from multer
+            const cloudFile = await cloudinary.uploader.upload(req.file?.path,{folder: "Alphacrunch/Users"})
+            // Retrieving profile pic cloudinary public id if it exists.
+            if (checkUser.profilePic_cloudId) {
+            await cloudinary.uploader.destroy(checkPic.profilePic_cloudId)
+            }
+            const profilePicture_url = cloudFile.secure_url;
+            const profilePic_cloudId = cloudFile.public_id;
 
-        // checking the cloudiinary upload from multer
-        const cloudFile = await cloudinary.uploader.upload(req.file.path,{folder: "Alphacrunch/Users"})
-        // Retrieving profile pic cloudinary public id if it exists.
-        if (checkUser.profilePic_cloudId) {
-           await cloudinary.uploader.destroy(checkPic.profilePic_cloudId)
+            const user = await User.findByIdAndUpdate(req.params.id,{fullName, sex, phoneNumber, city, state, address, profilePicture_url, profilePic_cloudId}, {new: true}).select("-password");
         }
-        const {fullName, sex, phoneNumber, city, state, address} = req.body;
-        const profilePicture_url = cloudFile.secure_url;
-        const profilePic_cloudId = cloudFile.public_id;
-
-        const user = await User.findByIdAndUpdate({ id: checkUser._id},{fullName, sex, phoneNumber, city, state, address, profilePicture_url, profilePic_cloudId}, {new: true}).select("-password");
+        const user = await User.findByIdAndUpdate(req.params.id,{fullName, sex, phoneNumber, city, state, address}, {new: true}).select("-password");
         return res.status(200).json({
             status: 'success',
             data: user
