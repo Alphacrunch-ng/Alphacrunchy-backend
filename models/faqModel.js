174cad3ee7b91e85857e2a59ace9362ce55cfcf9
moo-se <omedeomozuwa@gmail.com>
2024-01-18 13:16:06 +0100

    created faqSchema

---
+++ models/faqModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+
+const faqSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: ["true", "fullName is required."],
+    },
+    email: {
+      type: String,
+      required: ["true", "email is required."],
+      validate: [isEmail, "please enter a valid email"],
+    },
+    question: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    answer: {
+      type: String,
+      default: "",
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["General", "Billing", "Technical"],
+        message: "category can either be 'general' or 'billing' or 'technical'",
+      },
+      default: "General",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("FAQ", faqSchema);
+
+module.exports = FAQ;
