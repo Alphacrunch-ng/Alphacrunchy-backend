037017f2351a54c6d01b6d930c7fc2fd4fe864d9
Vastro-lorde <omatsolaseund@gmail.com>
2024-08-06 14:52:36 +0100

    changed passportjs callback to dynamic for live or dev

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,11 +1,16 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const { roles } = require('../utils/constants');
+const GOOGLE_SIGNUP_CALLBACK_URL = process.env.NODE_ENV === "production"? 
+"https://aphacrunch-api.onrender.com/auth/google/signup/callback" : "/auth/google/signup/callback";
+
+const GOOGLE_LOGIN_CALLBACK_URL = process.env.NODE_ENV === "production"? 
+"https://aphacrunch-api.onrender.com/auth/google/login/callback" : "/auth/google/login/callback"
 
 passport.use("google-signup",new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/signup/callback",
+    callbackURL: GOOGLE_SIGNUP_CALLBACK_URL,
     scope: ['profile', 'email', 'openid', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'],
   },
   async (token, tokenSecret, profile, done) => {
@@ -29,7 +34,7 @@ passport.use("google-signup",new GoogleStrategy({
 passport.use("google-login",new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/login/callback",
+  callbackURL: GOOGLE_LOGIN_CALLBACK_URL,
   scope: ['profile', 'email', 'openid', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'],
 },
 async (token, tokenSecret, profile, done) => {
